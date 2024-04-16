f5557ac023b21f416dc13eb8889fb50f3047f419
Sayyed Mohammad Miri <smmmiri@outlook.com>
2024-04-16 14:41:04 +0330

    Implemented MessagePublisher class

--- RabbitMQTurorialWebAPI.API/MessagePublisher.cs
+++ RabbitMQTurorialWebAPI.API/MessagePublisher.cs
@@ -1,9 +1,25 @@
-﻿namespace RabbitMQTurorialWebAPI.API;
+﻿using RabbitMQ.Client;
+using System.Text;
+using System.Text.Json;
+
+namespace RabbitMQTurorialWebAPI.API;
 
 public class MessagePublisher : IMessagePublisher
 {
     public void SendMessage<T>(T message)
     {
-        throw new NotImplementedException();
+        ConnectionFactory connectionFactory = new()
+        {
+            HostName = "localhost"
+        };
+        using var connection = connectionFactory.CreateConnection();
+        using var channel = connection.CreateModel();
+
+        channel.QueueDeclare("Orders");
+
+        var json = JsonSerializer.Serialize(message);
+        var body = Encoding.UTF8.GetBytes(json);
+
+        channel.BasicPublish(exchange: "", routingKey: "Orders", body: body);
     }
 }
