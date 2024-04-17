9bcabce76b57c181f34ce03eac5354a473e7fe5b
Sayyed Mohammad Miri <smmmiri@outlook.com>
2024-04-17 10:44:41 +0330

    Added MassTransit packages to the API and Console projects

---
+++ RabbitMQTurorialWebAPI.Common/Ticket.cs
@@ -0,0 +1,9 @@
+﻿namespace RabbitMQTurorialWebAPI.Common;
+
+public class Ticket
+{
+    public string UserName { get; set; } = Guid.NewGuid().ToString();
+    public DateTime BookedOn { get; set; }
+    public string? Boarding { get; set; }
+    public string? Destination { get; set; }
+}
